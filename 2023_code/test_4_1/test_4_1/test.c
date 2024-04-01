88d9e00ae59aa050691b28666ea2be9c225904c4
Y WSH <wsh18297826213@outlook.com>
2024-04-01 14:42:37 +0800

    上传代码

---
+++ 2023_code/test_4_1/test_4_1/test.c
@@ -0,0 +1,45 @@
+#define  _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// 用宏去写OFFSETOF，就是结构体成员的偏移量
+//struct S
+//{
+//	char c1;
+//	int a;
+//	char c2;
+//};
+//
+//#define OFFSETOF(struct_name,member_name) (int)&(((struct_name*)0)->member_name)
+//
+//int main()
+//{
+//	//struct S s;
+//	printf("%d\n", OFFSETOF(struct S, c1));
+//	printf("%d\n", OFFSETOF(struct S, a));
+//	printf("%d\n", OFFSETOF(struct S, c2));
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	union
+//	{
+//		short k;
+//		char i[2];
+//	}*s,a;
+//	s = &a;
+//	s->i[0] = 0x39;
+//	s->i[1] = 0x38;
+//	printf("%x\n", a.k);
+//	return 0;
+//}
+
+int main()
+{
+	int* p = (int*)malloc(0);
+
+	return 0;
+}
