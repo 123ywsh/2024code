38e730a3d8575d976f5dedf1710e1b36b3676120
Y WSH <wsh18297826213@outlook.com>
2024-04-01 18:28:41 +0800

    1

--- 2023_code/test_4_1/test_4_1/test.c
+++ 2023_code/test_4_1/test_4_1/test.c
@@ -37,9 +37,95 @@
 //	return 0;
 //}
 
+//int main()
+//{
+//	int* p = (int*)malloc(0);
+//
+//	return 0;
+//}
+
+//找单身狗
+//void Find(int arr[], int sz)
+//{
+//	int i = 0;
+//	int j = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		for (j = 0; j < sz; j++)
+//		{
+//			if ((i!=j)&&((arr[i] ^ arr[j]) == 0))
+//				arr[i] = 0;
+//		}
+//	}
+//}
+//
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,1,2,3,4,6 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	Find(arr,sz);
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	
+//	return 0;
+//}//不对，不会写,但知道了运算符优先级的问题
+
+//int main()
+//{
+//	int a = 1;
+//	int b = 2;
+//	printf("%d\n", a ^ b);
+//	if((a^b)==1)
+//		printf("%d\n", b);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 1;
+//	int b = 2;
+//	if ()
+//	{
+//		printf("hehe");
+//	}
+//	return 0;
+//}
+
+//看完解说之后的解法，重点！异或11,22,33,5，所有数，最后得到5，两个单身狗异或必有1，以此分组疑惑消掉有两个的就可以返回单身狗
+
+int* Find_singal_dog(int arr[], int sz, int* pd1, int* pd2)
+{
+	int i = 0;
+	int ret = 0;
+	for (i = 0; i < sz; i++)
+	{
+		ret ^= arr[i];
+	}
+	int pos = 0;
+	for (pos = 0; pos < 32; pos++)
+	{
+		if ((ret >> pos) & 1 == 1)
+			break;
+	}
+	for (i = 0; i < sz; i++)
+	{
+		if ((arr[i] >> pos) & 1 == 1)
+			*pd1 ^= arr[i];
+		else
+			*pd2 ^= arr[i];
+	}
+}
+
 int main()
 {
-	int* p = (int*)malloc(0);
-
+	int arr[] = { 1,2,3,4,5,1,2,3,4,6 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int dog1 = 0;
+	int dog2 = 0;
+	Find_singal_dog(arr, sz, &dog1, &dog2);
+	printf("dog1=%d dog2=%d\n", dog1, dog2);
 	return 0;
 }
